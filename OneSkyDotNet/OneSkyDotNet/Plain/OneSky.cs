d5646295894eaca57cefb69ab2f1f152173cacf8
QuadRatNewBy <vladimirgerasim@gmail.com>
2015-04-13 16:58:54 +0300

    Implementation start
    Authentication and Custom Request

---
+++ OneSkyDotNet/OneSkyDotNet/Plain/OneSky.cs
@@ -0,0 +1,67 @@
+﻿namespace OneSkyDotNet
+{
+    using System;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    internal class OneSky
+    {
+        private readonly string authenticationApiKey = "api_key";
+
+        private readonly string authenticationTimestamp = "timestamp";
+
+        private readonly string authenticationDevHash = "dev_hash";
+
+        private string publicKey;
+
+        private string secretKey;
+
+        public OneSky(string publicKey, string secretKey)
+        {
+            this.publicKey = publicKey;
+            this.secretKey = secretKey;
+        }
+
+        private int Timestamp
+        {
+            get
+            {
+                var origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+                var diff = DateTime.UtcNow - origin;
+                return (int)Math.Floor(diff.TotalSeconds);
+            }
+        }
+
+        public int RequestCount { get; private set; }
+
+        private string GetDevHash(int timestamp)
+        {
+            var md5 = MD5.Create();
+            var sb = new StringBuilder();
+
+            var input = timestamp + this.secretKey;
+            var inputBytes = Encoding.ASCII.GetBytes(input);
+            var hash = md5.ComputeHash(inputBytes);
+
+            foreach (var b in hash)
+            {
+                sb.Append(b.ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+
+        private OneSkyRequest CreateRequest(string url)
+        {
+            var timestamp = this.Timestamp;
+            var devHash = this.GetDevHash(timestamp);
+
+            this.RequestCount++;
+
+            return
+                (new OneSkyRequest(url)).Parameter(this.authenticationApiKey, this.publicKey)
+                    .Parameter(this.authenticationTimestamp, timestamp)
+                    .Parameter(this.authenticationDevHash, devHash);
+        }
+    }
+}
---
+++ OneSkyDotNet/OneSkyDotNet/Plain/OneSkyRequest.cs
@@ -0,0 +1,74 @@
+﻿namespace OneSkyDotNet
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    public class OneSkyRequest
+    {
+        private bool hasParams;
+
+        private string url;
+
+        private string contentType;
+
+        private IList<KeyValuePair<string, object>> body;
+
+        private IList<KeyValuePair<string, string>> files;
+
+        internal OneSkyRequest(string url)
+        {
+            this.hasParams = false;
+            this.body = new List<KeyValuePair<string, object>>();
+            this.files = new List<KeyValuePair<string, string>>();
+
+            var urlParam = url.Split(new[] { '?' });
+            var param = string.Join("?", urlParam.Skip(1)).Split(new[] { '&' });
+
+            this.url = urlParam[0];
+
+            foreach (var kvp in param.Select(s => s.Split(new[] { '=' })))
+            {
+                this.Parameter(kvp[0], kvp[1]);
+            }
+        }
+
+        public OneSkyRequest Placeholder(string placeholder, object value, bool condition = true)
+        {
+            if (condition)
+            {
+                this.url = this.url.Replace(string.Format("{{{0}}}", placeholder), value.ToString());
+            }
+            return this;
+        }
+
+        public OneSkyRequest Parameter(string parameter, object value, bool condition = true)
+        {
+            if (condition)
+            {
+                var prefix = this.hasParams ? '&' : '?';
+                this.url += string.Format("{0}{1}={2}", prefix, parameter, value);
+                this.hasParams = true;
+            }
+            return this;
+        }
+
+        public OneSkyRequest Body(string key, object value, bool condition = true)
+        {
+            if (condition)
+            {
+                this.body.Add(new KeyValuePair<string, object>(key, value));
+            }
+            return this;
+        }
+
+        public OneSkyRequest Files(string name, string path, bool condition = true)
+        {
+            if (condition)
+            {
+                this.files.Add(new KeyValuePair<string, string>(name, Path.GetFullPath(path)));
+            }
+            return this;
+        }
+    }
+}
