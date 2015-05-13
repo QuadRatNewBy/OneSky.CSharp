dfd3f116626e071e91546a5e0a94ca994b731e5f
QuadRatNewBy <vladimirgerasim@gmail.com>
2015-05-13 22:44:00 +0300

    [JSON] Client

---
+++ OneSkyDotNet/OneSkyDotNet/Json/OneSkyClient.cs
@@ -0,0 +1,14 @@
+﻿namespace OneSkyDotNet.Json
+{
+    public class OneSkyClient : IOneSkyClient
+    {
+        public static IOneSkyClient CreateClient(string publicKey, string secretKey)
+        {
+            return new OneSkyClient();
+        }
+
+        public IPlatform Platform { get; private set; }
+
+        public IPlugin Plugin { get; private set; }
+    }
+}
