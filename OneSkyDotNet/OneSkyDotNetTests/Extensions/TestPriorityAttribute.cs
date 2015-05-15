ff3e7d70739451e50bf5cd69606d9bfed8f0af66
QuadRatNewBy <vladimirgerasim@gmail.com>
2015-05-15 13:07:05 +0300

    Ordered test workaround

---
+++ OneSkyDotNet/OneSkyDotNetTests/Extensions/TestPriorityAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+public class TestPriorityAttribute : Attribute
+{
+    public TestPriorityAttribute(int priority)
+    {
+        this.Priority = priority;
+    }
+
+    public int Priority { get; private set; }
+}
---
+++ OneSkyDotNet/OneSkyDotNetTests/Platform.cs
@@ -0,0 +1,12 @@
+﻿namespace OneSkyDotNetTests
+{
+    using Xunit;
+
+    // This test class uses ordering to create everything from scratch.
+    // For example. Project.Create requires 'project_group_id' so we need to create ProjectGroup first.
+    [TestCaseOrderer("PriorityOrderer", "OneSkyDotNetTests")]
+    public class Platform
+    {
+         
+    }
+}
