87135aa635d3d5a31a6f5c9ae5461051bf5872fe
QuadRatNewBy <vladimirgerasim@gmail.com>
2015-05-26 16:44:06 +0300

    [JSON] Platform.Translation start

---
+++ OneSkyDotNet/OneSkyDotNet/Json/Objects/ITranslationStatus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneSkyDotNet.Json.Objects
+{
+    interface ITranslationStatus
+    {
+        string FileName { get; }
+
+        ILocale Locale { get; }
+
+        string Progress { get; }
+
+        int StringCount { get; }
+
+        int WordCount { get; }
+    }
+}
