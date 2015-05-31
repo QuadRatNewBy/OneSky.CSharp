d807171e82a705984198f27a819abbee4e85ab80
QuadRatNewBy <vladimirgerasim@gmail.com>
2015-05-31 14:24:55 +0300

    [JSON] Translation Status

---
+++ OneSkyDotNet/OneSkyDotNet/Json/Objects/TranslationStatus.cs
@@ -0,0 +1,32 @@
+﻿namespace OneSkyDotNet.Json
+{
+    using Newtonsoft.Json;
+
+    internal class TranslationStatus : ITranslationStatus
+    {
+        [JsonProperty("file_name")]
+        private string fileName;
+
+        [JsonProperty("locale")]
+        private ILocale locale;
+
+        [JsonProperty("progress")]
+        private string progress;
+
+        [JsonProperty("string_count")]
+        private int stringCount;
+
+        [JsonProperty("word_count")]
+        private int wordCount;
+
+        public string FileName { get { return this.fileName; } }
+
+        public ILocale Locale { get { return this.locale; } }
+
+        public string Progress { get { return this.progress; } }
+
+        public int StringCount { get { return this.stringCount; } }
+
+        public int WordCount { get { return this.wordCount; } }
+    }
+}
