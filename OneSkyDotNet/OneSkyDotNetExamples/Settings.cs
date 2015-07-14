e7ceb3b71fa09b222fd2fad2dec05cadb0a4f516
QuadRatNewBy <vladimirgerasim@gmail.com>
2015-07-14 18:04:09 +0300

    Example project removed (for now?)

--- OneSkyDotNet/OneSkyDotNetExamples/Settings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OneSkyDotNetExamples
-{
-    public static class Settings
-    {
-        public static string PublicKey = "zOBWzHPRI81rNQkj8IawLHArNrP840Tu";
-
-        public static string PrivateKey = "AVCQxVf3sbtjSOY3Gry1ycGdQg0xvNlV";
-    }
-}
