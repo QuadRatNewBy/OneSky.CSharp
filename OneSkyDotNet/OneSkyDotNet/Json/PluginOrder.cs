cc0d012398614d28a629067b37686c3104ee2c08
QuadRatNewBy <vladimirgerasim@gmail.com>
2015-07-14 17:11:49 +0300

    Plugin.Order

--- OneSkyDotNet/OneSkyDotNet/Json/PluginOrder.cs
+++ OneSkyDotNet/OneSkyDotNet/Json/PluginOrder.cs
@@ -1,6 +1,7 @@
 ﻿namespace OneSkyDotNet.Json
 {
     using System.Collections.Generic;
+    using System.Linq;
 
     internal class PluginOrder : IPluginOrder
     {
@@ -13,22 +14,49 @@
 
         public IOneSkyResponse<IMeta, IEnumerable<IOrderPlugin>> GetOrders(int projectId, int page = 1, int perPage = 15)
         {
-            throw new System.NotImplementedException();
+            var plain = this.order.GetOrders(projectId, page, perPage);
+            var tuple = JsonHelper.PluginDeserialize(plain, new { orders = new List<OrderPlugin>() }, x => x.orders);
+            return new OneSkyResponse<IMeta, IEnumerable<IOrderPlugin>>(
+                plain.StatusCode,
+                plain.StatusDescription,
+                tuple.Item1,
+                tuple.Item2);
         }
 
         public IOneSkyResponse<IMeta, IOrderPluginDetails> GetOrder(int projectId, int orderId)
         {
-            throw new System.NotImplementedException();
+            var plain = this.order.GetOrder(projectId, orderId);
+            var tuple = JsonHelper.PluginDeserialize(
+                plain,
+                new { orders = new List<OrderPluginDetails>() },
+                x => x.orders == null ? null : x.orders.FirstOrDefault());
+            return new OneSkyResponse<IMeta, IOrderPluginDetails>(
+                plain.StatusCode,
+                plain.StatusDescription,
+                tuple.Item1,
+                tuple.Item2);
         }
 
         public IOneSkyResponse<IMeta, IEnumerable<IItemDetails>> GetOrderItems(int projectId, int orderId)
         {
-            throw new System.NotImplementedException();
+            var plain = this.order.GetOrderItems(projectId, orderId);
+            var tuple = JsonHelper.PluginDeserialize(plain, new { items = new List<ItemDetails>() }, x => x.items);
+            return new OneSkyResponse<IMeta, IEnumerable<IItemDetails>>(
+                plain.StatusCode,
+                plain.StatusDescription,
+                tuple.Item1,
+                tuple.Item2);
         }
 
         public IOneSkyResponse<IMeta, IEnumerable<IMessage>> GetOrderMessages(int projectId, int orderId, int page = 1, int perPage = 15)
         {
-            throw new System.NotImplementedException();
+            var plain = this.order.GetOrderMessages(projectId, orderId, page, perPage);
+            var tuple = JsonHelper.PluginDeserialize(plain, new { messages = new List<Message>() }, x => x.messages);
+            return new OneSkyResponse<IMeta, IEnumerable<IMessage>>(
+                plain.StatusCode,
+                plain.StatusDescription,
+                tuple.Item1,
+                tuple.Item2);
         }
 
         public IOneSkyResponse<IMeta, IOrderPluginNew> PostOrders(
@@ -41,7 +69,21 @@
             bool isIncludingReview = false,
             string specialization = "general")
         {
-            throw new System.NotImplementedException();
+            var plain = this.order.PostOrders(
+                projectId,
+                fromLocale,
+                toLocales,
+                items,
+                tone,
+                note,
+                isIncludingReview,
+                specialization);
+            var tuple = JsonHelper.PluginDeserialize(plain, new { order = new OrderPluginNew() }, x => x.order);
+            return new OneSkyResponse<IMeta, IOrderPluginNew>(
+                plain.StatusCode,
+                plain.StatusDescription,
+                tuple.Item1,
+                tuple.Item2);
         }
     }
 }
