f3d1562535042524d72ef9b9350577a5d45dd7db
QuadRatNewBy <vladimirgerasim@gmail.com>
2015-07-16 17:26:46 +0300

    Plugin.Account test (credits)

---
+++ OneSkyDotNet/OneSkyDotNetTests/PluginAccount.cs
@@ -0,0 +1,22 @@
+﻿namespace OneSkyDotNetTests
+{
+    using FluentAssertions;
+
+    using Xunit;
+
+    public class PluginAccount
+    {
+        private OneSkyDotNet.Json.IPluginAccount account =
+            OneSkyDotNet.Json.OneSkyClient.CreateClient(Settings.PublicKey, Settings.PrivateKey).Plugin.Account;
+
+        [Fact]
+        public void Credits()
+        {
+            var response = this.account.GetCredit();
+
+            response.StatusCode.Should().BeInRange(200, 299);
+
+            response.DataContent.RemainingCredit.Should().BeGreaterOrEqualTo(0);
+        }
+    }
+}
