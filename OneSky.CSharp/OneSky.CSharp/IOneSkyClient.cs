d8b949b066b9e3d1259cd0eed6c0eb41a1e33a64
QuadRatNewBy <vladimirgerasim@gmail.com>
2015-07-23 13:55:32 +0300

    [XML Doc] IOneSkyClient.cs

--- OneSky.CSharp/OneSky.CSharp/IOneSkyClient.cs
+++ OneSky.CSharp/OneSky.CSharp/IOneSkyClient.cs
@@ -1,9 +1,19 @@
 ﻿namespace OneSky.CSharp
 {
+    /// <summary>
+    /// <para>OneSky API 'Plain' client interface.</para>
+    /// <para>Returns response body as <c>string</c>.</para>
+    /// </summary>
     public interface IOneSkyClient
     {
+        /// <summary>
+        /// Provides access to OneSky's 'Platform' API.
+        /// </summary>
         IPlatform Platform { get; }
 
+        /// <summary>
+        /// Provides access to OneSky's 'Plugin' API.
+        /// </summary>
         IPlugin Plugin { get; }
     }
 }
