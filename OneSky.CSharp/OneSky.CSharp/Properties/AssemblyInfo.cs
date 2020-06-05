3bbfa394c8db362d41af03c1a3b405a26e8c52e2
Uladzimir Herasimionak <vladimirgerasim@gmail.com>
2020-06-05 23:21:48 +0300

    NuGet package v1.2.0

--- OneSky.CSharp/OneSky.CSharp/Properties/AssemblyInfo.cs
+++ OneSky.CSharp/OneSky.CSharp/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("OneSky.CSharp")]
 [assembly: AssemblyDescription("C# client for OneSky API")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("QuadRatNewBy")]
+[assembly: AssemblyCompany("Uladzimir 'Kvadrat' Herasimionak")]
 [assembly: AssemblyProduct("OneSky .NET")]
-[assembly: AssemblyCopyright("Copyright © QuadRatNewBy 2016")]
+[assembly: AssemblyCopyright("Copyright © Uladzimir 'Kvadrat' Herasimionak 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersionAttribute("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersionAttribute("1.2.0.0")]
